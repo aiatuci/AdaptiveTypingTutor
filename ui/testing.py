4a03599e9a733a167385fd4c54b7d5c9771ff4f4
MinhTheMerciless <minhnx.vn@gmail.com>
2021-12-15 15:43:34 -0800

    Pasted keylog to testing

--- ui/testing.py
+++ ui/testing.py
@@ -1,6 +1,9 @@
+from os import read
 from typing import Tuple
 import keyboard
 import csv
+import pandas as pd
+from string import ascii_letters, punctuation
 
 times = []
 prev_time = 0
@@ -12,10 +15,14 @@ USER_ID = "user_id"
 def log_keys(text: str) -> list[Tuple[str, str, float]]:
   data: list[Tuple[str, float]] = []
 
+  allowed_chars: set[str] = set(
+    [p for p in punctuation] + [letters for letters in ascii_letters] + ['space', 'enter', 'backspace'] + [str(i) for i in range(10)]
+    )
+
   print('Type the following text: ' + text)
   while True:
     event: keyboard.KeyboardEvent = keyboard.read_event()
-    if event.event_type == "down":
+    if event.event_type == "down" and event.name in allowed_chars:
       data.append((event.name, event.event_type, event.time))
 
     if event.name == "esc":
@@ -35,5 +42,11 @@ def main():
   save_data(data, '69420')
   print(data)
 
+
+def get_data(user_id: str) -> pd.DataFrame:
+  return pd.read_csv(f"{user_id}.csv")
+
+
 if __name__ == "__main__":
   main()
+
