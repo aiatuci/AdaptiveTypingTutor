dffe178758b7c221364a99eca4c80fb3764daf23
MinhTheMerciless <minhnx.vn@gmail.com>
2021-12-15 15:41:16 -0800

    Keylog update

--- ui/keylog.py
+++ ui/keylog.py
@@ -1,6 +1,9 @@
+from os import read
 from typing import Tuple
 import keyboard
 import csv
+import pandas as pd
+from string import ascii_letters, punctuation
 
 times = []
 prev_time = 0
@@ -12,10 +15,14 @@ USER_ID = "user_id"
 def log_keys(text: str) -> list[Tuple[str, str, float]]:
   data: list[Tuple[str, float]] = []
 
+  allowed_chars: set[str] = set(
+    [p for p in punctuation] + [letters for letters in ascii_letters] + ['space', 'enter', 'backspace'] + [str(i) for i in range(10)]
+    )
+
   print('Type the following text: ' + text)
   while True:
     event: keyboard.KeyboardEvent = keyboard.read_event()
-    if event.event_type == "down":
+    if event.event_type == "down" and event.name in allowed_chars:
       data.append((event.name, event.event_type, event.time))
 
     if event.name == "esc":
@@ -34,6 +41,7 @@ def main():
   data = log_keys("Hello world!")
   save_data(data, '69420')
   print(data)
+
 
 if __name__ == "__main__":
   main()
